b302cc551e4d06f33b7318d89d0d721e400368a2
Clayton O'Neill <clayton@oneill.net>
2017-05-22 08:35:59 -0400

    Add compareHosts implementation
    This produces a list of route 53 records to update and another list to
    delete.

--- main.go
+++ main.go
@@ -34,8 +34,37 @@ func parseOpts() {
 	}
 }
 
-func compareHosts(hosts []hostEntry, r53hosts []hostEntry) {
+// compareHosts takes the contents of the local /etc/hosts file and the
+// route53 hosts that should be compared and produces two arrays.  The first
+// array is a list of Route53 records that need to be updated, and the second
+// array is a list of Route53 records that should be deleted.
+func compareHosts(hosts []hostEntry, r53hosts []hostEntry) ([]hostEntry, []hostEntry) {
+	// Build index on name, we'll delete entries out of here a we match them
+	// against /etc/hosts entries.  The remaining entries aren't present
+	// locally anymore and will need to be deleted.
+	rhByName := map[string]hostEntry{}
+	for _, rh := range r53hosts {
+		rhByName[rh.hostname] = rh
+	}
 
+	toUpdate := []hostEntry{}
+	// Find existing hosts
+	for _, h := range hosts {
+		rh, ok := rhByName[h.hostname]
+		if ok {
+			delete(rhByName, h.hostname)
+			if !h.ip.Equal(rh.ip) {
+				toUpdate = append(toUpdate, h)
+			}
+		}
+	}
+
+	toDelete := make([]hostEntry, 0, len(rhByName))
+	for _, rh := range rhByName {
+		toDelete = append(toDelete, rh)
+	}
+
+	return toUpdate, toDelete
 }
 
 func main() {
---
+++ main_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var cases = []struct {
+	name     string
+	hosts    []hostEntry
+	r53hosts []hostEntry
+	toUpdate []hostEntry
+	toDelete []hostEntry
+}{
+	{"noop",
+		[]hostEntry{
+			{hostname: "test1.test.com", ip: net.ParseIP("1.2.3.4")},
+		},
+		[]hostEntry{
+			{hostname: "test1.test.com", ip: net.ParseIP("1.2.3.4")},
+		},
+		[]hostEntry{},
+		[]hostEntry{},
+	},
+	{"need-update",
+		[]hostEntry{
+			{hostname: "test1.test.com", ip: net.ParseIP("1.2.3.4")},
+		},
+		[]hostEntry{
+			{hostname: "test1.test.com", ip: net.ParseIP("1.2.3.5")},
+		},
+		[]hostEntry{
+			{hostname: "test1.test.com", ip: net.ParseIP("1.2.3.4")},
+		},
+		[]hostEntry{},
+	},
+	{"remove-stale",
+		[]hostEntry{
+			{hostname: "test1.test.com", ip: net.ParseIP("1.2.3.4")},
+		},
+		[]hostEntry{
+			{hostname: "test1.test.com", ip: net.ParseIP("1.2.3.4")},
+			{hostname: "test2.test.com", ip: net.ParseIP("1.2.3.5")},
+		},
+		[]hostEntry{},
+		[]hostEntry{
+			{hostname: "test2.test.com", ip: net.ParseIP("1.2.3.5")},
+		},
+	},
+}
+
+func TestCompareHosts(t *testing.T) {
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			toUpdate, toDelete := compareHosts(c.hosts, c.r53hosts)
+			assert.Equal(t, c.toUpdate, toUpdate)
+			assert.Equal(t, c.toDelete, toDelete)
+		})
+	}
+}
